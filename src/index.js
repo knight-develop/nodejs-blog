a15abb919da0b78bb354a0bffc5484c94e72e112
knight-develop <anhkhai1222@gmail.com>
2021-05-06 23:42:13 +0700

    Refactor code: create routes, app/controller

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const route = require('./routes')
 const path = require('path');
 const port = 5000;
 //cau hinh folder static
 app.use(express.static(path.join(__dirname, 'public')));
+//logger
+app.use(morgan('combined'));
 //template engine
-app.engine('hbs', handlebars({extname:'.hbs'}));
+app.engine('hbs', handlebars({
+    extname:'.hbs'
+}));
 app.set('view engine', 'hbs');
 //config handlebars
 app.set('views', path.join(__dirname,'resources/views'));
-app.get('/', (req, res) => res.render('home'))
-app.get('/news', (req, res) => res.render('news'))
+//Route init
+route(app);
+
 app.listen(port,() => console.log(`listening on port ${port}`));
